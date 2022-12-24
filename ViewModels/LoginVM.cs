faf1816a5f31da26b0656dbb7125bb6169320a52
00-Veronika <ronij2000@gmail.com>
2022-12-24 11:40:22 +0200

    LoginVM

---
+++ ViewModels/LoginVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace virtualReality.ViewModels
+{
+    public class LoginVM
+    {
+            [DisplayName("Username: ")]
+            [Required(ErrorMessage = "*This field is Required!")]
+            public string Username { get; set; }
+
+            [DisplayName("Password: ")]
+            [Required(ErrorMessage = "*This field is Required!")]
+            public string Password { get; set; }
+
+            [DisplayName("Phone: ")]
+            [Required(ErrorMessage = "*This field is Required!")]
+            public string Phone { get; set; }
+        
+            [DisplayName("Email: ")]
+            [Required(ErrorMessage = "*This field is Required!")]
+            public string Email { get; set; }
+
+
+    }
+}

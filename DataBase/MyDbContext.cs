9b4c049ae60e58a9270c487b3a2c3c8c263827fc
00-Veronika <ronij2000@gmail.com>
2022-12-26 21:54:51 +0200

    not fixed

---
+++ DataBase/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntityFrameworkSample;
+
+namespace EntityFrameworkSample
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<User>Users { get; set; }
+        public MyDbContext()
+        {
+            Users = this.Set<User>();
+        }
+        protected override void OnConfiguring(DbContextOptions Builder optionBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost; DataBase= VirtualRealityDb;");
+        }
+    }
+}
